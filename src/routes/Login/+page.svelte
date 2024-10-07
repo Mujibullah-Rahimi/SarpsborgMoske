95a791776612dc57cda5e9a631bd23d2a51bccc7
Mujibullah <mujibbullah@hotmail.no>
2024-10-07 11:34:43 +0200

    login page update

--- src/routes/Login/+page.svelte
+++ src/routes/Login/+page.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
 	import { goto } from "$app/navigation";
 	import LeftMcc from "$lib/icons/left-mcc.svelte";
+	import { loginUser } from "../Login/auth";
+	
 
     let email = "";
     let password = "";
-    
+
     function goHome(){
         goto("/");
     }
+
+    async function handleLogin() {
+		// isLoading = true;
+		// errorMessage = '';
+		await loginUser(email, password);
+		setTimeout(() => {
+			// isLoading = false;
+		}, 3000);
+	}
 
 </script>
 
@@ -27,7 +38,7 @@
 			<h1>Logg inn</h1>
 		</div>
 
-		<form>
+		<form on:submit={handleLogin}>
 			<input type="email" bind:value={email} placeholder="Epost" required autocomplete="email" />
 			<input
 				type="password"
